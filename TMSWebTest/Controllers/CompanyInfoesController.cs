5902acc91a5210023f9e62dad8dfd9925f5c8b09
Mohamed Elkholy <mohamed.elkholy@unit4.com>
2021-01-18 20:34:45 +0100

    test

---
+++ TMSWebTest/Controllers/CompanyInfoesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TMSWebTest.Models;
+
+namespace TMSWebTest.Controllers
+{
+    public class CompanyInfoesController : Controller
+    {
+        private MiltonDBEntities db = new MiltonDBEntities();
+
+        // GET: CompanyInfoes
+        public ActionResult Index()
+        {
+            return View(db.CompanyInfoes.ToList());
+        }
+
+        // GET: CompanyInfoes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CompanyInfo companyInfo = db.CompanyInfoes.Find(id);
+            if (companyInfo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(companyInfo);
+        }
+
+        // GET: CompanyInfoes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CompanyInfoes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,CompanyName,VAT_ID_NIP,Phone,Mobile,Address,Website,Email,Branch,City,Country,AccountId,Notes,Logo")] CompanyInfo companyInfo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.CompanyInfoes.Add(companyInfo);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(companyInfo);
+        }
+
+        // GET: CompanyInfoes/Edit/5
+        public ActionResult Edit(int? id=1)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CompanyInfo companyInfo = db.CompanyInfoes.Find(id);
+            if (companyInfo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(companyInfo);
+        }
+
+        // POST: CompanyInfoes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,CompanyName,VAT_ID_NIP,Phone,Mobile,Address,Website,Email,Branch,City,Country,AccountId,Notes,Logo")] CompanyInfo companyInfo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(companyInfo).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(companyInfo);
+        }
+
+        // GET: CompanyInfoes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CompanyInfo companyInfo = db.CompanyInfoes.Find(id);
+            if (companyInfo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(companyInfo);
+        }
+
+        // POST: CompanyInfoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            CompanyInfo companyInfo = db.CompanyInfoes.Find(id);
+            db.CompanyInfoes.Remove(companyInfo);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ TMSWebTest/Global.asax
@@ -0,0 +1,1 @@
+<%@ Application Codebehind="Global.asax.cs" Inherits="TMSWebTest.MvcApplication" Language="C#" %>
